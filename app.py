9d090a96b23c1070fc3af787c2ccfbbc4cacd4c1
Flaminia <flaminic@thoughtworks.com>
2018-07-19 19:04:48 -0300

    adding forms examples

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,29 @@ def welcome():
 @app.route("/template")
 def template():
     return render_template('index.html')
+
+
+@app.route("/profiles")
+def profiles():
+    usuarios = ['celi', 'belu', 'flami', 'santa claus']
+    return render_template('profiles.html', usuarios = usuarios)
+
+
+@app.route("/profile/<nombre>")
+def profile(nombre):
+    return render_template('profile.html', first_name = nombre)
+
+@app.route("/register-profile")
+def register_profile():
+    name = request.args['firstName']
+    name2 = request.args['lastName']
+    password = request.args['password']
+    nascita = request.args['birthdate']
+    return render_template('register-profile.html', name=name, name2 = name2, nascita=nascita)
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
